f2ff580c56a31927da11b831ef800a6257133ae2
Khristopher <khriss201403@gmail.com>
2025-01-27 13:23:56 -0500

    Agregaste nuevos archivos.py

---
+++ Ejercicios de curso CISCO/listas/compresion.py/tablero_ajedrez.py
@@ -0,0 +1,10 @@
+board = [["9" for i in range(8)] for j in range(8)]
+
+board[0][0] = "ROOK"
+board[0][7] = "ROOK"
+board[7][0] = "ROOK"
+board[7][7] = "ROOK"
+board[4][2] = "KNIGHT"
+board[3][4] = "PAWN"
+
+print(board)
