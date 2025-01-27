2e70961d79ec82931b18e63412d5f026b93f8ad1
Khristopher <khriss201403@gmail.com>
2025-01-27 10:45:50 -0500

    Agrrgué archivos nuevos

--- Ejercicios de curso CISCO/listas/ordenamiento_burbuja.py
+++ Ejercicios de curso CISCO/listas/ordenamiento_burbuja.py
@@ -1,3 +1,5 @@
+# Este es el ordenamiento burbuja que se usa para enseñar a ordenar comparando el valor adiacente.
+
 Lista = [30,12,67,24,1,27,100,-5,98]
 activar = True
 while activar:
@@ -8,3 +10,15 @@ while activar:
             activar = True
     
 print(Lista)
+
+# Este es un método en Python que permite ordenar una lista con sort().
+my_list = [8, 10, 6, 2, 4]
+my_list.sort()
+print(my_list)
+
+# También existe este otro método llamado reverse() que se usa para invertir la lista.
+lst = [5, 3, 1, 2, 4]
+print(lst)
+
+lst.reverse()
+print(lst)  # output: [4, 2, 1, 3, 5]
